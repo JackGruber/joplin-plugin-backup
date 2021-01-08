8288c5e6deb55ea606e8d7bfb3429ee7b8e62a65
JackGruber <24863925+JackGruber@users.noreply.github.com>
2021-01-08 23:39:19 +0100

    Add basci backup

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import joplin from "api";
 import JoplinCommands from "api";
 import { MenuItem, MenuItemLocation, SettingItemType } from "api/types";
 
+const fs = require("fs-extra");
+
 joplin.plugins.register({
   onStart: async function () {
     console.info("Backup plugin started");
@@ -24,6 +26,36 @@ joplin.plugins.register({
       label: "Create Backup",
       execute: async () => {
         console.info("Start backup");
+
+        const backupPath = await joplin.settings.value("path");
+        if (fs.existsSync(backupPath)) {
+          let noteBooks = {};
+          let pageNum = 0;
+          do {
+            var folders = await joplin.data.get(["folders"], {
+              fields: "id, title, parent_id",
+              limit: 50,
+              page: pageNum++,
+            });
+            for (const folder of folders.items) {
+              noteBooks[folder.id] = {};
+              noteBooks[folder.id]["title"] = folder.title;
+              noteBooks[folder.id]["parent_id"] = folder.parent_id;
+            }
+          } while (folders.has_more);
+
+          for (const folder of folders.items) {
+            let name: string = await getNotebookFileName(noteBooks, folder.id);
+            await joplin.commands.execute(
+              "exportFolders",
+              folder.id,
+              "jex",
+              backupPath + "/" + name
+            );
+          }
+        } else {
+          console.info("Backup Path '" + backupPath + "' does not exist");
+        }
 
         console.info("End backup");
       },
