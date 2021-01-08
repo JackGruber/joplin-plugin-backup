3b8c33712109c2adb0be2aa3cbc9a92bf0fc805a
JackGruber <24863925+JackGruber@users.noreply.github.com>
2021-01-08 22:50:23 +0100

    Add backupSection

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
 import joplin from "api";
 import JoplinCommands from "api";
-import { MenuItem, MenuItemLocation } from "api/types";
+import { MenuItem, MenuItemLocation, SettingItemType } from "api/types";
 
 joplin.plugins.register({
   onStart: async function () {
     console.info("Backup plugin started");
+
+		await joplin.settings.registerSection('backupSection', {
+			label: 'Backup',
+			iconName: 'fas fa-archive',
+		});
+		
+		await joplin.settings.registerSetting('path', {
+			value: 123,
+			type: SettingItemType.String,
+			section: 'backupSection',
+			public: true,
+			label: 'Backup Path',
+		});
 
     await joplin.commands.register({
       name: "CreateBackup",
