d075b7b2642f564cdfb9e7763b1fa878cad17dcb
JackGruber <24863925+JackGruber@users.noreply.github.com>
2021-01-14 16:34:16 +0100

    Automatic backup

--- src/index.ts
+++ src/index.ts
@@ -31,6 +31,17 @@ joplin.plugins.register({
       label: "Keep x Backups",
     });
 
+    await joplin.settings.registerSetting("backupInterval", {
+      value: 24,
+      minimum: 0,
+      maximum: 999,
+      type: SettingItemType.Int,
+      section: "backupSection",
+      public: true,
+      label: "Backups interval in hours",
+      description: "0 = disable automatic backup",
+    });
+
     await joplin.settings.registerSetting("lastBackup", {
       value: 0,
       type: SettingItemType.Int,
@@ -40,133 +51,13 @@ joplin.plugins.register({
     });
 
     const backupDialog = await joplin.views.dialogs.create("backupDialog");
+    const startTime = new Date();
 
     await joplin.commands.register({
       name: "CreateBackup",
       label: "Create Backup",
       execute: async () => {
-        console.info("Start backup");
-
-        const baseBackupPath = await joplin.settings.value("path");
-
-        if (fs.existsSync(baseBackupPath)) {
-          let backupPath = baseBackupPath;
-          const backupDate = new Date();
-          const backupRetention = await joplin.settings.value(
-            "backupRetention"
-          );
-          if (backupRetention > 1) {
-            backupPath =
-              baseBackupPath +
-              "/" +
-              backupDate.getFullYear().toString() +
-              (backupDate.getMonth() + 1).toString().padStart(2, "0") +
-              backupDate.getDate().toString().padStart(2, "0") +
-              backupDate.getHours().toString().padStart(2, "0") +
-              backupDate.getMinutes().toString().padStart(2, "0") +
-              backupDate.getSeconds().toString().padStart(2, "0");
-            try {
-              fs.mkdirSync(backupPath);
-            } catch (e) {
-              showError("Backup error", e);
-              throw e;
-            }
-
-            // delete old backup sets
-            const oldBackupSets = fs
-              .readdirSync(baseBackupPath, { withFileTypes: true })
-              .filter((dirent) => dirent.isDirectory())
-              .map((dirent) => dirent.name)
-              .reverse();
-            for (let i = backupRetention; i < oldBackupSets.length; i++) {
-              try {
-                fs.rmdirSync(baseBackupPath + "/" + oldBackupSets[i], {
-                  recursive: true,
-                });
-              } catch (e) {
-                showError("Backup error", e);
-                throw e;
-              }
-            }
-          } else {
-            const oldBackupData = fs
-              .readdirSync(baseBackupPath, { withFileTypes: true })
-              .filter((dirent) => dirent.isFile())
-              .map((dirent) => dirent.name)
-              .reverse();
-            for (const file of oldBackupData) {
-              try {
-                fs.removeSync(baseBackupPath + "/" + file);
-              } catch (e) {
-                showError("Backup error", e);
-                throw e;
-              }
-            }
-          }
-
-          let noteBooks = {};
-          let pageNum = 0;
-          do {
-            var folders = await joplin.data.get(["folders"], {
-              fields: "id, title, parent_id",
-              limit: 50,
-              page: pageNum++,
-            });
-            for (const folder of folders.items) {
-              noteBooks[folder.id] = {};
-              noteBooks[folder.id]["title"] = folder.title;
-              noteBooks[folder.id]["parent_id"] = folder.parent_id;
-            }
-          } while (folders.has_more);
-
-          // Backup notebooks with notes
-          for (const folder of folders.items) {
-            let noteCheck = await joplin.data.get(
-              ["folders", folder.id, "notes"],
-              {
-                fields: "title, id",
-              }
-            );
-            if (noteCheck.items.length > 0) {
-              let name: string = await getNotebookFileName(
-                noteBooks,
-                folder.id
-              );
-              try {
-                console.info("Backup >>" + folder.title + "<< (" + folder.id + ") as >>" + name + "<<");
-                let status: string = await joplin.commands.execute(
-                  "exportFolders",
-                  folder.id,
-                  "jex",
-                  backupPath + "/" + name
-                );
-              } catch (e) {
-                showError("Backup error", e);
-                throw e;
-              }
-              console.info(status);
-            }
-          }
-
-          // Backup Keymap
-          const profileDir = await joplin.settings.globalValue("profileDir");
-          try {
-            fs.copyFileSync(
-              profileDir + "/keymap-desktop.json",
-              backupPath + "/keymap-desktop.json"
-            );
-          } catch (e) {
-            showError("Backup error", e);
-            throw e;
-          }
-          
-          await joplin.settings.setValue('lastBackup', backupDate.valueOf());
-        } else {
-          console.info("Backup Path '" + baseBackupPath + "' does not exist");
-        }
-
-        showError("Backup finished", "The backup was created");
-        console.info("End backup");
+        await startBackup(true);
       },
     });
 
@@ -175,6 +66,135 @@ joplin.plugins.register({
       "CreateBackup",
       MenuItemLocation.Tools
     );
+
+    async function startBackup(showMsg) {
+      console.info("Start backup");
+
+      const baseBackupPath = await joplin.settings.value("path");
+
+      if (fs.existsSync(baseBackupPath)) {
+        let backupPath = baseBackupPath;
+        const backupDate = new Date();
+        const backupRetention = await joplin.settings.value("backupRetention");
+        if (backupRetention > 1) {
+          backupPath =
+            baseBackupPath +
+            "/" +
+            backupDate.getFullYear().toString() +
+            (backupDate.getMonth() + 1).toString().padStart(2, "0") +
+            backupDate.getDate().toString().padStart(2, "0") +
+            backupDate.getHours().toString().padStart(2, "0") +
+            backupDate.getMinutes().toString().padStart(2, "0") +
+            backupDate.getSeconds().toString().padStart(2, "0");
+          try {
+            fs.mkdirSync(backupPath);
+          } catch (e) {
+            showError("Backup error", e);
+            throw e;
+          }
+
+          // delete old backup sets
+          const oldBackupSets = fs
+            .readdirSync(baseBackupPath, { withFileTypes: true })
+            .filter((dirent) => dirent.isDirectory())
+            .map((dirent) => dirent.name)
+            .reverse();
+          for (let i = backupRetention; i < oldBackupSets.length; i++) {
+            try {
+              fs.rmdirSync(baseBackupPath + "/" + oldBackupSets[i], {
+                recursive: true,
+              });
+            } catch (e) {
+              showError("Backup error", e);
+              throw e;
+            }
+          }
+        } else {
+          const oldBackupData = fs
+            .readdirSync(baseBackupPath, { withFileTypes: true })
+            .filter((dirent) => dirent.isFile())
+            .map((dirent) => dirent.name)
+            .reverse();
+          for (const file of oldBackupData) {
+            try {
+              fs.removeSync(baseBackupPath + "/" + file);
+            } catch (e) {
+              showError("Backup error", e);
+              throw e;
+            }
+          }
+        }
+
+        let noteBooks = {};
+        let pageNum = 0;
+        do {
+          var folders = await joplin.data.get(["folders"], {
+            fields: "id, title, parent_id",
+            limit: 50,
+            page: pageNum++,
+          });
+          for (const folder of folders.items) {
+            noteBooks[folder.id] = {};
+            noteBooks[folder.id]["title"] = folder.title;
+            noteBooks[folder.id]["parent_id"] = folder.parent_id;
+          }
+        } while (folders.has_more);
+
+        // Backup notebooks with notes
+        for (const folder of folders.items) {
+          let noteCheck = await joplin.data.get(
+            ["folders", folder.id, "notes"],
+            {
+              fields: "title, id",
+            }
+          );
+          if (noteCheck.items.length > 0) {
+            let name: string = await getNotebookFileName(noteBooks, folder.id);
+            try {
+              console.info(
+                "Backup >>" +
+                  folder.title +
+                  "<< (" +
+                  folder.id +
+                  ") as >>" +
+                  name +
+                  "<<"
+              );
+              let status: string = await joplin.commands.execute(
+                "exportFolders",
+                folder.id,
+                "jex",
+                backupPath + "/" + name
+              );
+            } catch (e) {
+              showError("Backup error", e);
+              throw e;
+            }
+          }
+        }
+
+        // Backup Keymap
+        const profileDir = await joplin.settings.globalValue("profileDir");
+        try {
+          fs.copyFileSync(
+            profileDir + "/keymap-desktop.json",
+            backupPath + "/keymap-desktop.json"
+          );
+        } catch (e) {
+          showError("Backup error", e);
+          throw e;
+        }
+
+        await joplin.settings.setValue("lastBackup", backupDate.getTime());
+      } else {
+        console.info("Backup Path '" + baseBackupPath + "' does not exist");
+      }
+
+      if (showMsg === true) {
+        showError("Backup finished", "The backup was created");
+      }
+      console.info("End backup");
+    }
 
     async function showError(title, e) {
       await joplin.views.dialogs.setButtons(backupDialog, [{ id: "ok" }]);
@@ -206,5 +226,28 @@ joplin.plugins.register({
           .replace(/[[/\?%*:|"<>]]/g, "_") + ".jex"
       );
     }
+
+    async function checkBackupTime() {
+      console.info("Check for timed backup");
+      const lastBackup = await joplin.settings.value("lastBackup");
+      const backupInterval = await joplin.settings.value("backupInterval");
+      const now = new Date();
+      const checkEver = 5;
+
+      if (backupInterval > 0) {
+        // Do not start backup directly after startup
+        if (startTime.getTime() + (checkEver - 1) * 60 * 1000 < now.getTime()) {
+          if (now.getTime() > lastBackup + backupInterval * 60 * 60 * 1000) {
+            await startBackup(false);
+          }
+        }
+
+        window.setTimeout(checkBackupTime, 1000 * 60 * checkEver);
+      } else {
+        console.info("Automatic backup disabled");
+      }
+    }
+
+    checkBackupTime();
   },
 });
