8541fa72c8347462f653778cb89e3c25032fd30e
JackGruber <24863925+JackGruber@users.noreply.github.com>
2021-06-12 17:41:29 +0200

    zip add function

--- src/sevenZip.ts
+++ src/sevenZip.ts
@@ -12,7 +12,38 @@ export namespace sevenZip {
     );
   }
 
-  async function add() {}
+  export async function add(
+    archive: string,
+    src: string,
+    password: string = null,
+    options: string[] = null
+  ): Promise<any> {
+    let _7zOptions = ["a", archive, src];
+
+    if (options) {
+      _7zOptions = [..._7zOptions, ...options];
+    }
+
+    if (password) {
+      _7zOptions.push("-p" + password);
+      _7zOptions.push("-mhe");
+    }
+
+    const promise = new Promise((resolve, reject) => {
+      _7z.cmd(_7zOptions, (err) => {
+        if (err) return reject(err);
+        resolve(true);
+      });
+    });
+
+    return await promise
+      .then((data) => {
+        return data;
+      })
+      .catch((err) => {
+        return err.message;
+      });
+  }
 
   async function list() {}
 
