3e51c622a32335f700f3f3819ebd183f0f4d1290
JackGruber <24863925+JackGruber@users.noreply.github.com>
2022-11-02 19:23:09 +0100

    Add versionCompare

--- __test__/help.test.ts
+++ __test__/help.test.ts
@@ -71,4 +71,79 @@ describe("Test helper", function () {
       );
     }
   });
+  it(`Compare versions`, async () => {
+    const testCases = [
+      {
+        version1: "2.9.12",
+        version2: "2.9.12",
+        expected: 0,
+      },
+      {
+        version1: "2.9.12",
+        version2: "2.9.13",
+        expected: -1,
+      },
+      {
+        version1: "2.9.13",
+        version2: "2.9.12",
+        expected: 1,
+      },
+      {
+        version1: "2.10.6",
+        version2: "2.9.12",
+        expected: 1,
+      },
+      {
+        version1: "3.10.6",
+        version2: "2.11.8",
+        expected: 1,
+      },
+      {
+        version1: "2.10.6",
+        version2: "3.11.8",
+        expected: -1,
+      },
+      {
+        version1: "2",
+        version2: "2.1",
+        expected: -1,
+      },
+      {
+        version1: "2.1",
+        version2: "2",
+        expected: 1,
+      },
+      {
+        version1: "3",
+        version2: "2",
+        expected: 1,
+      },
+      {
+        version1: "2",
+        version2: "2",
+        expected: 0,
+      },
+      {
+        version1: "3.11.8",
+        version2: "3.11.8-a",
+        expected: 0,
+      },
+      {
+        version1: "2",
+        version2: "",
+        expected: -2,
+      },
+      {
+        version1: "3.a.8",
+        version2: "3.11.8",
+        expected: -1,
+      },
+    ];
+
+    for (const testCase of testCases) {
+      expect(
+        await helper.versionCompare(testCase.version1, testCase.version2)
+      ).toBe(testCase.expected);
+    }
+  });
 });
