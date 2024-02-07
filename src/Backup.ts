b4f7612b4cc5954c110aa69da7258675bb7c6b51
Henry H <personalizedrefrigerator@gmail.com>
2024-02-07 11:13:15 -0800

    Feat: Add a README file to the root backup directory

--- src/Backup.ts
+++ src/Backup.ts
@@ -477,6 +477,7 @@ class Backup {
         await this.backupNotebooks();
 
         const backupDst = await this.makeBackupSet();
+        await this.writeReadme(backupDst);
 
         await joplin.settings.setValue(
           "lastBackup",
@@ -682,6 +683,16 @@ class Backup {
     } else {
       return false;
     }
+  }
+
+  private async writeReadme(backupFolder: string) {
+    const readmePath = path.join(backupFolder, "README.md");
+    this.log.info("writeReadme to", readmePath);
+    const readmeText = i18n.__(
+      "backupReadme",
+      this.backupStartTime.toLocaleString()
+    );
+    await fs.writeFile(readmePath, readmeText, "utf8");
   }
 
   private async backupNotebooks() {
